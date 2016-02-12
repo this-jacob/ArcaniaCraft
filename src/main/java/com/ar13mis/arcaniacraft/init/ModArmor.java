30a95d99e2ab3fc4efe7d9526fda3461db71650b
Jacob Tuttle <kingtut5474@gmail.com>
2016-02-11 19:35:59 -0700

    Mystical Armor

--- src/main/java/com/ar13mis/arcaniacraft/init/ModArmor.java
+++ src/main/java/com/ar13mis/arcaniacraft/init/ModArmor.java
@@ -1,21 +1,36 @@
 package com.ar13mis.arcaniacraft.init;
 
+import com.ar13mis.arcaniacraft.item.armor.ItemMysticalArmor;
 import com.ar13mis.arcaniacraft.item.armor.ItemStrangeArmor;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModArmor
 {
 
+    //Strange Armor Creation
     public static ItemStrangeArmor strangehelmet = new ItemStrangeArmor(ModArmorMaterials.strangeArmorMaterial, 0, "strangehelmet");
     public static ItemStrangeArmor strangechestplate = new ItemStrangeArmor(ModArmorMaterials.strangeArmorMaterial, 1, "strangechestplate");
     public static ItemStrangeArmor strangeleggings = new ItemStrangeArmor(ModArmorMaterials.strangeArmorMaterial, 2, "strangeleggings");
     public static ItemStrangeArmor strangeboots = new ItemStrangeArmor(ModArmorMaterials.strangeArmorMaterial, 3, "strangeboots");
 
+    //Mystical Armor Creation
+    public static ItemMysticalArmor mysticalhelmet = new ItemMysticalArmor(ModArmorMaterials.mysticalArmorMaterial, 0, "mysticalhelmet");
+    public static ItemMysticalArmor mysticalchestplate = new ItemMysticalArmor(ModArmorMaterials.mysticalArmorMaterial, 1, "mysticalchestplate");
+    public static ItemMysticalArmor mysticalleggings = new ItemMysticalArmor(ModArmorMaterials.mysticalArmorMaterial, 2, "mysticalleggings");
+    public static ItemMysticalArmor mysticalboots = new ItemMysticalArmor(ModArmorMaterials.mysticalArmorMaterial, 3, "mysticalboots");
+
     public static void init()
     {
+        //Strange Armor Registration
         GameRegistry.registerItem(strangehelmet, "strangehelmet");
         GameRegistry.registerItem(strangechestplate, "strangechestplate");
         GameRegistry.registerItem(strangeleggings, "strangeleggings");
         GameRegistry.registerItem(strangeboots, "strangeboots");
+
+        //Mystical Armor Registration
+        GameRegistry.registerItem(mysticalhelmet, "mysticalhelmet");
+        GameRegistry.registerItem(mysticalchestplate, "mysticalchestplate");
+        GameRegistry.registerItem(mysticalleggings, "mysticalleggings");
+        GameRegistry.registerItem(mysticalboots, "mysticalboots");
     }
 }
---
+++ src/main/java/com/ar13mis/arcaniacraft/item/armor/ItemMysticalArmor.java
@@ -0,0 +1,44 @@
+package com.ar13mis.arcaniacraft.item.armor;
+
+import com.ar13mis.arcaniacraft.creativetab.CreativeTabArcaniaCraft;
+import com.ar13mis.arcaniacraft.init.ModArmor;
+import com.ar13mis.arcaniacraft.reference.Reference;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+public class ItemMysticalArmor extends ItemArmor {
+    public ItemMysticalArmor(ItemArmor.ArmorMaterial localArmorMaterial, int armorType, String name)
+    {
+
+        super(localArmorMaterial, 0, armorType);
+
+        setUnlocalizedName(Reference.MOD_ID + ":" + name);
+
+        setTextureName(Reference.MOD_ID + ":" + name);
+
+        setCreativeTab(CreativeTabArcaniaCraft.ARC_TAB);
+
+    }
+
+    //Defines the textures for the armor
+    public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
+    {
+
+        if(stack.getItem() == ModArmor.mysticalhelmet || stack.getItem() == ModArmor.mysticalchestplate || stack.getItem() == ModArmor.mysticalboots)
+        {
+
+            return Reference.MOD_ID + ":models/armor/strangearmor1.png";
+
+        }else if(stack.getItem() == ModArmor.mysticalleggings){
+
+            return Reference.MOD_ID + ":models/armor/strangearmor2.png";
+
+        }else{
+
+            System.out.println("Invalid Item");
+            return null;
+
+        }
+    }
+}
