054feeb70a9134481c72e6e41735de676577e4ad
Jacob Tuttle <kingtut5474@gmail.com>
2016-02-12 18:52:03 -0700

    Added strange armor crafting recipe

--- src/main/java/com/ar13mis/arcaniacraft/recipes/Armor/strangearmor.java
+++ src/main/java/com/ar13mis/arcaniacraft/recipes/Armor/strangearmor.java
@@ -1,7 +1,10 @@
 package com.ar13mis.arcaniacraft.recipes.Armor;
 
+import com.ar13mis.arcaniacraft.init.ModArmor;
+import com.ar13mis.arcaniacraft.init.ModItems;
 import com.ar13mis.arcaniacraft.recipes.Recipes;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
 
 public class strangearmor extends Recipes
 {
@@ -9,7 +12,25 @@ public class strangearmor extends Recipes
     {
 
         //Register strange armor crafting recipes here
-        //GameRegistry.addShapedRecipe();
+        //Strange Helmet Recipe
+        GameRegistry.addShapedRecipe(new ItemStack(ModArmor.strangehelmet),
+                "sss", "s s", "   ",
+                's', new ItemStack(ModItems.strangeingot));
+
+        //Strange Chestplate Recipe
+        GameRegistry.addShapedRecipe(new ItemStack(ModArmor.strangechestplate),
+                "s s", "sss", "sss",
+                's', new ItemStack(ModItems.strangeingot));
+
+        //Strange Leggings Recipe
+        GameRegistry.addShapedRecipe(new ItemStack(ModArmor.strangeleggings),
+                "sss", "s s", "s s",
+                's', new ItemStack(ModItems.strangeingot));
+
+        //Strange Boots Recipe
+        GameRegistry.addShapedRecipe(new ItemStack(ModArmor.strangeboots),
+                "   ", "s s", "s s",
+                's', new ItemStack(ModItems.strangeingot));
 
     }
 }
